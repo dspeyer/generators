3779574ac9f3f1e75e4db90e912e222b3bade76c
dspeyer <dspeyer@gmail.com>
2013-03-13 23:25:28 -0400

    add a very simple example

---
+++ Makefile
@@ -0,0 +1,2 @@
+range_example: range_example.cc generators.h
+	g++ -o range_example range_example.cc -std=c++11 -pthread
---
+++ range_example.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"generators.h"
+
+struct Range : GeneratorHeart<int> {
+  int run(int start, int end, int interval=1) {
+    for (int i=start; i<end; i+=interval) {
+      yield(i);
+    }
+  }
+};
+
+main() {
+  auto iter = Gen<Range>(1,10);
+  do {
+    std::cout << *iter << std::endl;
+  }while(++iter);
+  for (int i : Gen<Range>(42,53,2)) {
+    std::cout << i << std::endl;
+  }
+  for (int i : Gen<Range>(-5,0)) {
+    std::cout << i << std::endl;
+    if (i == -2) break;
+  }
+}
